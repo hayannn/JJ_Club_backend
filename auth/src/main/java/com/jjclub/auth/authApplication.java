7db60c80c81795a95ca1aef8e4fbc5d71981d62d
hayannn <102213509+hayannn@users.noreply.github.com>
2023-05-23 18:54:27 +0900

    fix(인증): 메인 커밋과 연결

--- auth/src/main/java/com/jjclub/auth/authApplication.java
+++ auth/src/main/java/com/jjclub/auth/authApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+//메인 클래스
 @EnableJpaAuditing
 @SpringBootApplication
 public class authApplication {
